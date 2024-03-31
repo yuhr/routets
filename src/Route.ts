2e54b52be2d4a152aa177bdb66aaac0b961ee90e
Sʜɪᴍᴜʀᴀ Yū <mail@yuhr.org>
2024-04-01 02:39:25 +0900

    refactor!: remove deprecated features

--- src/Route.ts
+++ src/Route.ts
@@ -1,14 +1,10 @@
 namespace Route {
 	export type Captured = Record<string, string | undefined>
-	/** @deprecated Use {@link Captured `Captured`} instead. */
-	export type Slugs = Record<string, string | undefined>
 	export type Context = {
 		/** The original `Request` object. */
 		request: Request
 		/** The captured substrings of the pathname. */
 		captured: Captured
-		/** @deprecated Use {@link Context.captured `captured`} instead. */
-		slugs: Slugs
 		/** The relative path to the route file from the serving root directory, including the suffix and the extension. */
 		path: string
 		/** A clone of the instance of `URLPattern` that is used to match this route. */
