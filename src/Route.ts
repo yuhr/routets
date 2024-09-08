ac67be5be0987f602ea5bf6487f7b711ca2f6710
Sʜɪᴍᴜʀᴀ Yū <mail@yuhr.org>
2024-09-08 11:19:12 +0900

    improvement!: change default behavior of `Router` constructor (#21)
    - `root` and `watch`: relative path `string`s are resolved from the call
    site of `Router` constructor (so you can pass `"."` instead of
    `import.meta.resolve("./.")`)
    - `write`: `false` by default
    - The CLI accepts `--import-map` and `Router` constructor accepts
    `importMap`
    - `reloads` is no longer available in route handlers; use `router.watch`
    instead

--- src/Route.ts
+++ src/Route.ts
@@ -1,5 +1,8 @@
+import type Router from "./Router.ts"
+
 namespace Route {
 	export type Captured = Record<string, string | undefined>
+
 	export type Context = {
 		/** The original `Request` object. */
 		request: Request
@@ -9,28 +12,35 @@ namespace Route {
 		path: string
 		/** A clone of the instance of `URLPattern` that is used to match this route. */
 		pattern: URLPattern
-		/** An `AsyncIterable` that yields when the browser is needed to be reloaded as per watch. */
-		reloads: AsyncIterable<void>
+		/** The `Router` instance that is serving the route. */
+		router: Router
 	}
+
 	export type Handler<
 		ReturnType = Response | void,
 		AdditionalParameters extends readonly unknown[] = [],
 	> = (context: Context, ...additionalParameters: AdditionalParameters) => Promise<ReturnType>
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 interface Route extends Route.Handler {}
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 class Route {
 	constructor(handler: Route.Handler) {
-		return Object.defineProperty(
-			Object.setPrototypeOf(handler, new.target.prototype),
-			Symbol.for("routets"),
-			{ value: undefined, configurable: false, enumerable: false, writable: false },
-		)
+		return Object.defineProperty(Object.setPrototypeOf(handler, new.target.prototype), symbol, {
+			value: undefined,
+			configurable: false,
+			enumerable: false,
+			writable: false,
+		})
 	}
+
 	static isRoute(value: unknown): value is Route {
-		return typeof value === "function" && Symbol.for("routets") in value
+		return typeof value === "function" && symbol in value
 	}
 }
+
+const symbol = Symbol()
 
 export default Route
